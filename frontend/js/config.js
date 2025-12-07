539ae15fc45aa67856a0e1c90038660852420efe
hadiabsi22-ctrl <hadiabsi22@gmail.com>
2025-12-07 03:45:05 +0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,8 +1,10 @@
 const API_CONFIG = {
-    BASE_URL: "https://reviewqeem-backend.onrender.com/api",
+    BASE_URL: "https://reviewqeem-backend-1.onrender.com/api",
+
     REVIEWS: "/reviews",
     GAMES: "/games",
     ADMIN_LOGIN: "/admin/auth/login"
 };
 
 // test
+
