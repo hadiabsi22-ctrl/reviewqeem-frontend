e42774c8f4a2c2787345ac50e1203ccc3bb86d3e
hadiabsi22-ctrl <hadiabsi22@gmail.com>
2025-12-08 19:56:37 +0300

    Update review-view.js

--- frontend/js/review-view.js
+++ frontend/js/review-view.js
@@ -2,7 +2,8 @@
 // قائمة المراجعات - متكامل مع API + متوافق مع review-view و review-management
 
 (function () {
-  BASE_URL: "http://84.247.170.23:5000/api",
+  const API_BASE = "http://84.247.170.23:5000/api";
+
 
 
   // نخزن المراجعات هنا عشان تقدر تستخدمها صفحات ثانية
@@ -354,6 +355,7 @@
     fetchReviews();
   });
 })();
+
 
 
 
